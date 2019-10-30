559d672cef643a06ef4171e21530a3d24b116394
Rick Lopez <ricklopezmba@outlook.com>
2019-10-30 11:07:59 -0500

    update

--- src/adlConfig.js
+++ src/adlConfig.js
@@ -3,9 +3,9 @@ import { AuthenticationContext, adalFetch, withAdalLogin } from 'react-adal';
 export const adalConfig =  {
     tenant: '9abc9a0d-17f0-4f74-9195-ec797d5dd726',
     clientId: 'fe92e92e-24c3-4eab-870b-49b1308d46b9',
-    redirectUri: 'http://localhost:3000/dashboard',
+    redirectUri: process.env.NODE_ENV === 'production' ? 'https://qqmigration-dev.qqsolutions.com/dashboard' :  'http://localhost:3000/dashboard',
     endpoints: {
-        api: 'https://localhost:44307/api/User/current',
+        api:  process.env.NODE_ENV === 'production' ? `${process.env.REACT_APP_AUTH_ROOT_URL}/api/v1/User/current` : 'https://localhost:44307/api/User/current'
     },
     cacheLocation: 'sessionStorage'
 };
