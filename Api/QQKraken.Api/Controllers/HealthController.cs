3a3baf5e1da445839af5f5e47617d81d67e3d41d
Rick Lopez <ricklopezmba@outlook.com>
2019-11-10 11:54:35 -0600

    Update for deployment. Add non auth health endpoint

---
+++ Api/QQKraken.Api/Controllers/HealthController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace QQKraken.Api.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public HealthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // GET: api/Health
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Health/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Health/DB
+        [HttpGet]
+        [Route("db")]
+        public string Detdb()
+        {
+            try
+            {
+                using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+                {
+                    connection.Open();
+                    var state = connection.State;
+
+                    return (state == ConnectionState.Open) ? "DB State Open OK" : "DB State NOT OPEN";
+                }
+            }
+            catch (Exception ex) {
+                return "Connection could not open to DefaultConnection";
+            }
+           
+        }
+
+        // PUT: api/Health/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
